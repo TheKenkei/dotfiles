6e3d9bf96879291b226795e88b452f8e753674a0
TheKenkei <nowikow.witaliy@gmail.com>
2024-05-15 00:32:41 +0300

    feat(zsh): update configs, add new alias

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,4 +1,11 @@
+#GLOBAL 
+alias v="nvim"
 alias lg="lazygit" 
+alias update="source ~/.zshrc"
+
+#DOCKER
+alias dc="docker compose" 
+alias drestart="docker compose down ; docker compose up -d "
 
 alias precommit="npm run eslint ; npm run prettier && npm run test && npm run typecheck && npm run build && echo COMPLETE!!!"
 alias npm-clear="rm -rf ~/.npm; rm -rf ~/.cache"
@@ -9,3 +16,4 @@ alias t-nvim-config="tmux new -s nvim-config -c ~/.config/nvim || tmux a -t nvim
 
 alias update="source ~/.zshrc"
 alias brew-update="brew update -d --auto-update --verbose --force --debug"
+alias cdd="cd $1 && ls"
