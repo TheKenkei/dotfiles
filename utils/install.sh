5b8378d8f179f57af9694e5c85e0101bb3b186a8
TheKenkei <nowikow.witaliy@gmail.com>
2024-05-16 18:38:58 +0300

    chore: update `install.sh`

--- utils/install.sh
+++ utils/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
 ln -s ${HOME}/.config/zsh/zshrc ${HOME}/.zshrc
+ln -s ${HOME}/.config/zsh/oh-my-zsh ${HOME}/.oh-my-zsh
+ln -s ${HOME}/.config/zsh/p10k.zsh ${HOME}/.p10k.zsh
+
 ln -s ${HOME}/.config/tmux ${HOME}/.tmux
 ln -s ${HOME}/.config/tmux/tmux.conf ${HOME}/.tmux.conf
